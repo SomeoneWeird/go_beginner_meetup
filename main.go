dae36adaf278ce39cde85409a1ac55aa2af8e7e8
Adam Brady <adam@boxxen.org>
2015-03-03 19:07:57 +1100

    check we have no errors

--- main.go
+++ main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+// http://api.openweathermap.org/data/2.5/weather?q=Melbourne,au
 
 func main() {
 
-	fmt.Printf("Hello, world!\n")
+	_, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=Melbourne,au")
+
+	if err != nil {
+		fmt.Printf("Error getting weather: %v", err)
+		return
+	}
 
 }
