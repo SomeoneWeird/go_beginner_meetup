7e5a0fb94ad8344f31d213abf738af03ac5c8070
Adam Brady <adam@boxxen.org>
2015-03-03 19:22:52 +1100

    add weather and city struct, move main body into function

--- main.go
+++ main.go
@@ -9,23 +9,38 @@ import (
 // http://api.openweathermap.org/data/2.5/weather?q=Melbourne,au
 
 func main() {
+	body, _ := getWeatherResponseBody()
+	fmt.Printf("Response: %s", body)
+}
+
+func getWeatherResponseBody() ([]byte, error) {
 
 	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=Melbourne,au")
 
 	if err != nil {
 		fmt.Printf("Error getting weather: %v", err)
-		return
+		return []byte(""), err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
 		fmt.Printf("Error reading weather: %v", err)
-		return
+		return []byte(""), err
 	}
 
 	defer resp.Body.Close()
 
-	fmt.Printf("Response: %s", body)
+	return body, nil
 
+}
+
+type City struct {
+	Weather Weather `json:"main"`
+	Name    string  `json:"name"`
+}
+
+type Weather struct {
+	CurrentTemp float64 `json:"temp"`
+	MaxTemp     float64 `json:"mtepm_max"`
 }
